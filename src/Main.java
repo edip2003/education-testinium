a86b744a5e853bf85b164e73c06c09f924825ee0
edip.ismail <edip.ismail@testinium.com>
2025-11-03 15:54:19 +0300

    toplama and carpım

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,35 @@ public class Main {
     public static void main(String[] args) {
 
 
-
-
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Kullnaıcı adını giriniz :");
-        String userName =scanner.nextLine();
+        String userName = scanner.nextLine();
         System.out.println("Şifreyi Giriniz: ");
-        String password =scanner.nextLine();
-        if ("admin".equals(userName)&&"1234".equals(password)){
+        String password = scanner.nextLine();
+        if ("admin".equals(userName) && "1234".equals(password)) {
             System.out.println("Giriş Başarılı!!");
-        }else{
+        } else {
             System.out.println("Hatalı Giriş!");
-        }
-    }
 
+        }
+
+       sum();
+
+    }
+    // İkinci Çalışma
+    public static void sum(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("First Number: ");
+        int firstNum = input.nextInt();
+        System.out.println("Second Number: ");
+        int secondNum = input.nextInt();
+        int sum = firstNum + secondNum;
+        System.out.println("Sum "+ sum);
+        int mult = firstNum * secondNum;
+        System. out. println("mult "+ mult);
+        if (mult > 5000 & sum < 4000) {
+            System. out. println("Koşul sağlandı" + mult);
+        }
+
+    }
 }
