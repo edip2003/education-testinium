4f3976fc2b4191fb701e0aca9dac8b5de2e8c3fe
edip.ismail <edip.ismail@testinium.com>
2025-11-03 14:48:37 +0300

    logşn and password user name

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome1!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Kullnaıcı adını giriniz :");
+        String userName =scanner.nextLine();
+        System.out.println("Şifreyi Giriniz: ");
+        String password =scanner.nextLine();
+        if ("admin".equals(userName)&&"1234".equals(password)){
+            System.out.println("Giriş Başarılı!!");
+        }else{
+            System.out.println("Hatalı Giriş!");
+        }
     }
+
 }
