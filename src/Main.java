8d7b3112e45981a6797080ffdfbef215e5d0466b
edip.ismail <edip.ismail@testinium.com>
2025-11-03 15:14:39 +0300

    sum and mult

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
-
-
+       /* // Birinci Çalışma
         Scanner scanner=new Scanner(System.in);
         System.out.println("Kullnaıcı adını giriniz :");
         String userName =scanner.nextLine();
@@ -15,7 +13,29 @@ public class Main {
             System.out.println("Giriş Başarılı!!");
         }else{
             System.out.println("Hatalı Giriş!");
-        }
-    }
+        }*/
 
+        // İkinci Çalışma
+        twoNumber();
+
+
+
+    }
+    // İkinci Çalışma
+    public static void twoNumber(){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("First Number:");
+        int firstNum=scanner.nextInt();
+        System.out.println("Second Number:");
+        int secondNum=scanner.nextInt();
+        int sum =firstNum+secondNum;
+        System.out.println("Sum :"+sum);
+        int mult=firstNum*secondNum;
+        System.out.println("Mult:"+mult);
+        if (mult>5000||sum<4000){
+            System.out.println("Koşul sağlandı :"+mult);
+
+        }
+
+    }
 }
